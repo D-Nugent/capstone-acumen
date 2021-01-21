591d9156710c52d35f92926dddb3549125b02300
David Nugent <davienugent@hotmail.com>
2021-01-20 20:00:39 -0800

    CAPSTONE-10: Boilerplate Pages and components - Small update to style sheets for pages, linked to global partial scss - Additional re-used components added for future implementation

---
+++ front-end/src/components/ActionNewEntry/ActionNewEntry.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ActionNewEntry() {
+    return (
+        <div>
+            <p>Plus Icon to Add a new entry</p>
+        </div>
+    )
+}
+
+export default ActionNewEntry
---
+++ front-end/src/components/VideoPlayer/VideoPlayer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class VideoPlayer extends Component {
+    render() {
+        return (
+            <div>
+                <p>Video Player</p>
+            </div>
+        )
+    }
+}
+
+export default VideoPlayer
