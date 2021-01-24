414b3d103fbe379184c69f9f72e42ccf2a5dfb40
David A. Nugent <73457386+D-Nugent@users.noreply.github.com>
2021-01-24 10:19:04 -0800

    Capstone 14 (#6)
    * Initial commit
    - Will switch current changes to branch CAPSTONE-12
    
    * Initial Database Build added with firebase
    - Removed react-firebase and built db with firebase instead

--- front-end/src/components/SideNav/SideNav.jsx
+++ front-end/src/components/SideNav/SideNav.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './SideNav.scss'
 
 export class SideNav extends Component {
+
     render() {
         return (
             <div className="sidenav">
